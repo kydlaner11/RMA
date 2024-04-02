b1c5bfddc75c87065b08b92043fa3176c211d7d5
Renaldy <mickael.kristianto@spectrumindo.com>
2024-04-02 10:24:56 +0700

    Update dataTable + Update fitur Create Ticket

--- src/constant/columns/ticket.jsx
+++ src/constant/columns/ticket.jsx
@@ -1,12 +1,22 @@
 import {
-  ArrowUpOutlined,
+  ExclamationCircleOutlined,
   InfoCircleOutlined,
-  PlusOutlined,
-  QuestionOutlined,
+  CheckCircleOutlined
 } from "@ant-design/icons";
 import { Avatar, Button, Tag } from "antd";
 import { accountAbility } from "../../utils/ability";
 import { getColumnSearchProps } from "../../utils/column";
+import moment from "moment";
+
+
+// Fungsi untuk menentukan apakah suatu tanggal sudah melewati periode garansi
+const isOutOfWarranty = (warrantyEndDate) => {
+  // Misalnya, kita asumsikan tanggal sekarang adalah hari ini
+  const currentDate = moment();
+  const warrantyEnd = moment(warrantyEndDate);
+  
+  return currentDate.isAfter(warrantyEnd); // Mengembalikan true jika sudah melewati periode garansi
+};
 
 const statusOptions = [
   {
@@ -34,33 +44,36 @@ const speciesOptions = [
   },
 ];
 
-const genderOptions = [
+const warrantyOptions = [
   {
-    text: "Male",
-    value: "Male",
+    text: "Out of Warranty",
+    value: "Oow",
   },
   {
-    text: "Female",
-    value: "Female",
-  },
-  {
-    text: "unknown",
-    value: "unknown",
+    text: "Warranty Period",
+    value: "Wp",
   },
 ];
 
 export const charactersColumn = ({ searchProps }) =>
   [
     {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-      width: "14px",
+      title: "Ticket RMA",
+      dataIndex: "no_tickets",
+      key: "no_tickets",
+      ...getColumnSearchProps("no_tickets", searchProps),
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (_, record) => (
+        <div>
+          {record.no_tickets}
+        </div>
+      ),
     },
     {
       title: "Device",
-      key: "device",
-      ...getColumnSearchProps("name", searchProps),
+      dataIndex: "product_name",
+      key: "product_name",
+      ...getColumnSearchProps("product_name", searchProps),
       sorter: (a, b) => a.name.localeCompare(b.name),
       render: (_, record) => (
         <div>
@@ -69,7 +82,7 @@ export const charactersColumn = ({ searchProps }) =>
             alt={record.name}
             style={{ marginRight: "8px" }}
           />{" "}
-          {record.name}
+          {record.product_name}
         </div>
       ),
     },
@@ -78,39 +91,65 @@ export const charactersColumn = ({ searchProps }) =>
       dataIndex: "mac_address",
       key: "mac_address",
       filters: speciesOptions,
-      onFilter: (value, record) => record.species.indexOf(value) === 0,
-    },
-    {
-      title: "Warranty",
-      key: "warranty",
-      filters: genderOptions,
-      onFilter: (value, record) => record.gender.indexOf(value) === 0,
+      ...getColumnSearchProps("mac_address", searchProps),
+      sorter: (a, b) => a.name.localeCompare(b.name),
       render: (_, record) => (
-        <Tag
-          icon={
-            record.gender === "Male" ? (
-              <ArrowUpOutlined rotate={45} />
-            ) : record.gender === "Female" ? (
-              <PlusOutlined />
-            ) : (
-              <QuestionOutlined />
-            )
-          }
-          color={
-            record.gender === "Male"
-              ? "blue"
-              : record.gender === "Female"
-              ? "pink"
-              : "orange"
-          }
-        >
-          {record.gender}
-        </Tag>
+        <div>
+          {record.mac_address}
+        </div>
       ),
     },
     {
+      title: "Warranty",
+      dataIndex: "warranty",
+      key: "warranty",
+      filters: warrantyOptions,
+      onFilter: (value, record) => record.warranty === value,
+      render: (_, record) => {
+        const isExpired = isOutOfWarranty(record.warrantyEndDate);
+        return (
+          <Tag
+            icon={isExpired ? <ExclamationCircleOutlined /> : <CheckCircleOutlined />}
+            color={isExpired ? "red" : "green"}
+          >
+            {isExpired ? record.warranty : record.warranty}
+          </Tag>
+        );
+      },
+    },
+    // {
+    //   title: "Warranty",
+    //   dataIndex: "warranty",
+    //   key: "warranty",
+    //   filters: warrantyOptions,
+    //   onFilter: (value, record) => record.gender.indexOf(value) === 0,
+    //   render: (_, record) => (
+    //     <Tag
+    //       icon={
+    //         record.warranty === "Male" ? (
+    //           <ArrowUpOutlined rotate={45} />
+    //         ) : record.warranty === "Female" ? (
+    //           <PlusOutlined />
+    //         ) : (
+    //           <QuestionOutlined />
+    //         )
+    //       }
+    //       color={
+    //         record.warranty === "Male"
+    //           ? "blue"
+    //           : record.warranty === "Female"
+    //           ? "pink"
+    //           : "orange"
+    //       }
+    //     >
+    //       {record.warranty}
+    //     </Tag>
+    //   ),
+    // },
+    {
       title: "Status",
-      key: "status",
+      dataIndex: "status_ticket",
+      key: "status_ticket",
       filters: statusOptions,
       onFilter: (value, record) => record.status.indexOf(value) === 0,
       render: (_, record) => (
