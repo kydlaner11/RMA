4168718405caefec9742e7f6523a1d48dd8f843b
Ariani Kartika Suwignyo <ariani.suwignyo@dwp.co.id>
2024-06-14 09:12:18 +0000

    Update url.jsx staging

--- src/constant/url.jsx
+++ src/constant/url.jsx
@@ -9,7 +9,7 @@ const url = {
   },
   staging: {
     // BASE_URL_BE: "https://rational-wealthy-panther.ngrok-free.app/",
-    BASE_URL_BE: "https://jaguar-ready-naturally.ngrok-free.app",
+    BASE_URL_BE: "https://trialrma-be.dwp.io",
     BASE_URL_PUSHER: "https://d9af-202-148-7-130.ngrok.io",
     BASE_URL_FE: "https://trialrma.dwp.io",
   },
